400aa71ec2919fffa1b5832e2e711a20284185b0
namjiyun <namjiyun3275@gmail.com>
2022-05-23 10:13:51 +0900

    [Fix] 모듈 import

--- src/routes/wishRouter.ts
+++ src/routes/wishRouter.ts
@@ -1,14 +1,10 @@
-import { Router } from "express";
-import { body } from "express-validator/check";
-import { wishController } from "../controllers";
-import wishController from '../controllers/wishController';
+import { Router } from 'express';
+import { body } from 'express-validator/check';
+import { wishController } from '../controllers';
 
 const router: Router = Router();
 
-router.post('/', [
-    body("name").notEmpty(),
-], wishController.createWish);
-
+router.post('/', [body('name').notEmpty()], wishController.createWish);
 
 router.post('/:wishId', wishController.postRoomInWish);
 export default router;
