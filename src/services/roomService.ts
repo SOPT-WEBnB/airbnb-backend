a4e5a75e4a1e12009a191b8db8fdd286989d9c2d
namjiyun <namjiyun3275@gmail.com>
2022-05-25 10:10:06 +0900

    [Fix]

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -15,18 +15,7 @@ const postRoomInWish = async (roomId: string, wishId: string) => {
 const searchRoomInWish = async (wishId: string) => {
   try {
     const data = await Wish.findById(wishId).populate('rooms');
-    // var arr: Array<object | null> = [];
 
-    // data.rooms.map(async (room: any) => {
-    //   const data2 = await Room.findById(room.room);
-    //   arr.push(data2);
-    // });
-    // const result = {
-    //   _id: data._id,
-    //   name: data.name,
-    //   rooms: arr,
-    // };
-    // console.log(arr);
     console.log(data);
     return data;
   } catch (error) {
