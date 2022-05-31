2ea4a70398165b3a1d5eb0bef56055a7b7521ca5
namjiyun <namjiyun3275@gmail.com>
2022-05-31 22:29:08 +0900

    [Fix] cors

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ const app = express();
 import connectDB from './loaders/db';
 import routes from './routes';
 require('dotenv').config();
-import cors from 'cors';
+const cors = require('cors');
+
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,13 @@ app.use(express.json());
 app.use(cors());
 app.use(routes); //라우터
 // error handler
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
+  next();
+});
 
 interface ErrorType {
   message: string;
