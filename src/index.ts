32c0c27b5213f72e8e2f37d888475d60ecb01ca0
namjiyun <namjiyun3275@gmail.com>
2022-05-31 22:32:00 +0900

    [Feat]

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,6 @@ app.use(cors());
 app.use(routes); //라우터
 // error handler
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
-  next();
-});
-
 interface ErrorType {
   message: string;
   status: number;
